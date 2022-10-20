92c0c650fff97cc2ea532b4d3b50fb6c7bc62598
Mahesh <maheshkarande1985@gmail.com>
2022-10-20 11:57:48 +0530

    childscrollviewadded

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,88 +36,110 @@ class _HomeScreenState extends State<HomeScreen> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-          appBar: AppBar(
-            // elevation: 4,
-            title: const Text('Home'),
-            leading: AppBarIcons(
-              function: () {},
-              icon: IconlyBold.category,
-            ),
-            actions: [
-              AppBarIcons(
-                function: () {},
-                icon: IconlyBold.user3,
-              ),
-            ],
+        appBar: AppBar(
+          // elevation: 4,
+          title: const Text('Home'),
+          leading: AppBarIcons(
+            function: () {},
+            icon: IconlyBold.category,
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 18,
-                ),
-                TextField(
-                  controller: _textEditingController,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                      hintText: "Search",
-                      filled: true,
-                      fillColor: Theme.of(context).cardColor,
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          color: Theme.of(context).cardColor,
-                        ),
+          actions: [
+            AppBarIcons(
+              function: () {},
+              icon: IconlyBold.user3,
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 18,
+              ),
+              TextField(
+                controller: _textEditingController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        color: Theme.of(context).cardColor,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(
-                          width: 1,
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.secondary,
                       ),
-                      suffixIcon: Icon(
-                        IconlyLight.search,
-                        color: lightIconsColor,
-                      )),
-                ),
-                SizedBox(
-                  height: size.height * 0.25,
-                  child: Swiper(
-                    itemCount: 3,
-                    itemBuilder: (ctx, index) {
-                      return const SaleWidget();
-                    },
-                    autoplay: true,
-                    pagination: const SwiperPagination(
-                        alignment: Alignment.bottomCenter,
-                        builder: DotSwiperPaginationBuilder(
-                            color: Colors.white, activeColor: Colors.red)),
-                    // control: const SwiperControl(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+                    ),
+                    suffixIcon: Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    )),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      const Text(
-                        "Latest Products",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (ctx, index) {
+                            return const SaleWidget();
+                          },
+                          autoplay: true,
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                          // control: const SwiperControl(),
                         ),
                       ),
-                      const Spacer(),
-                      AppBarIcons(
-                          function: () {}, icon: IconlyBold.arrowRight2),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "Latest Products",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const Spacer(),
+                            AppBarIcons(
+                                function: () {}, icon: IconlyBold.arrowRight2),
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: 3,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 0.0,
+                                  mainAxisSpacing: 0.0,
+                                  childAspectRatio: 0.6),
+                          itemBuilder: (context, index) {
+                            return const FeedsWidget();
+                          })
                     ],
                   ),
                 ),
-                const SizedBox(width: 160, child: FeedsWidget()),
-              ],
-            ),
-          )),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
